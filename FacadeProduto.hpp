5360a9fb6fd32a1762b39b3305763d0f261cb114
Plinio Lima <70240238+pliinio@users.noreply.github.com>
2024-09-12 22:53:09 -0300

    Adicionando arquivos novamente
    Implementação do padrão Command

---
+++ FacadeProduto.hpp
@@ -0,0 +1,41 @@
+#ifndef FACADEPRODUTO_HPP
+#define FACADEPRODUTO_HPP
+
+#include "Produto.hpp"
+#include "Observer.hpp"
+#include "CaixaObserver.hpp"
+#include <algorithm>
+#include <vector>
+#include <string>
+
+class FacadeProduto {
+public:
+    static FacadeProduto& getInstance();
+
+    void loadProdutos();  
+    void saveProdutos();  
+
+    void cadastrarProduto(const Produto& produto);
+    void entradaMaterial(int id, int quantidadeAdicional);
+    Produto* buscarProduto(int id);
+    void exibirEstoque();
+    bool existsNomeProduto(const std::string& nome);
+    bool existsProduto(int id);
+    void vendaProduto(void);
+    Produto* buscarProdutoPorNome(const std::string& nome);
+
+    void addObserver(Observer* observer);
+    void removeObserver(Observer* observer);
+
+
+
+private:
+    FacadeProduto() { loadProdutos(); }
+    std::vector<Produto> produtos;
+    std::vector<Observer*> observers;
+
+    void notifyObservers(float valorVenda);
+    
+};
+
+#endif
